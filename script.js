a37182804ee0a8146a24f9c7016bf89c4140763a
Kacper Żarna <kacper-zarna@wp.pl>
2022-09-06 10:33:54 +0200

    Kod 27.08.2022

--- script.js
+++ script.js
@@ -33,6 +33,9 @@ const XMLGetRequest = () => {
 const FetchGetRequest = function () {
   // fetch domyslnie dziala na metodzie GET
   // fetch zwraca Promise
+  // const options = {
+  //   method: 'DELETE'
+  // }
   fetch(`https://jsonplaceholder.typicode.com/posts/1`)
     .then((response) => {
       console.log(response);
@@ -99,8 +102,8 @@ const FetchPostRequest = () => {
   };
 
   fetch("https://jsonplaceholder.typicode.com/posts", options).then((res) => {
-    console.log(res.status);
-    console.log(res);
+    // console.log(res.status);
+    // console.log(res);
   });
   // .then((res) => {
   //   return res.json();
@@ -111,22 +114,162 @@ FetchPostRequest();
 
 // c) Fetch API + async/await
 const FetchPOSTAsync = async () => {
-    const options = {
-      method: "POST",
-      body: JSON.stringify({
-        title: "Async/await and POST requests.",
-        body: "Async/await syntax can help you a lot making AJAX requests!",
-        userId: 3,
-      }),
-    };
-    try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts",
-        options
-      );
-      const responseData = await response.json();
-      console.log(responseData);
-    } catch (err) {
-      console.error(err.message);
-    }
+  const options = {
+    method: "POST",
+    body: JSON.stringify({
+      title: "Async/await and POST requests.",
+      body: "Async/await syntax can help you a lot making AJAX requests!",
+      userId: 3,
+    }),
   };
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/5",
+      options
+    );
+    const responseData = await response.json();
+    console.log(responseData);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+// 3. Metoda DELETE
+// a) XMLHttpRequest
+// b) Fetch API + then chain
+// c) Fetch API + async/await
+
+// Napisz 3 requesty z metodą delete przy pomocy 3 wypisanych wyżej rozwiązań, w rozwiązaniach z Fetch API pamiętaj o obiekcie options.
+
+// 4. Metoda PUT
+// a) XMLHttpRequest
+// b) Fetch API + then chain
+// c) Fetch API + async/await
+
+// Napisz 3 requesty z metodą delete przy pomocy 3 wypisanych wyżej rozwiązań, w rozwiązaniach z Fetch API pamiętaj o obiekcie options.
+
+// PUT
+// Aktualizacja i nadpisywanie istniejących danych, jeżeli dane nie istnieją wcześniej, to je doda
+
+// POST
+// dodaje nowe dane i nie może nadpisywać
+
+// 4. Metoda PATCH
+// a) XMLHttpRequest
+// b) Fetch API + then chain
+// c) Fetch API + async/await
+
+// Napisz 3 requesty z metodą delete przy pomocy 3 wypisanych wyżej rozwiązań, w rozwiązaniach z Fetch API pamiętaj o obiekcie options.
+
+// PATCH
+// bliźniacza do PUTa
+
+// OBIEKT W BAZIE DANYCH
+// {
+//   {
+//    id: 5,
+//    name: 'abc',
+//    age: 30
+//   }
+// }
+
+// PUT {
+//   id: 5,
+//   name: 'abc',
+//   age: 20
+// }
+
+// PATCH {
+//   age: 20
+// }
+
+const fetchStocks = async () => {
+  const response = await fetch(
+    `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=MOF905T490J40QKZ`
+  );
+  const data = await response.json();
+  const tables = Object.entries(data["Time Series (5min)"]);
+  const dates = tables.map((el) => el[0]);
+  const values = tables.map((el) => el[1]);
+  const lowValues = values.map((el) => +el["3. low"]);
+  const highValues = values.map((el) => +el["2. high"]);
+  const min = Math.min(...lowValues);
+  const max = Math.max(...highValues);
+  const amplitude = max - min;
+  console.log(`Amplitude: ${amplitude}`);
+};
+
+//fetchStocks();
+
+// https://www.alphavantage.co
+// /query?
+// function=TIME_SERIES_INTRADAY
+// &symbol=IBM
+// &interval=5min
+// &apikey=MOF905T490J40QKZ
+
+// const person = {
+//   name: "John",
+//   job: "Truck driver",
+//   age: 40,
+// };
+
+// console.log(person);
+// // console.log(Object.entries(person));
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
+
+// const arr = [1, 2, 3, 4, 5];
+// const arrTimes2 = arr.map((el, i, arr) => {
+//   return el * 2;
+// });
+// console.log(arrTimes2);
+
+const fetchFootballData = async () => {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-Auth-Token": "1f020942fbbc4285b9af9c61c35c8ef5",
+    },
+  };
+
+  const res = await fetch(
+    "https://api.football-data.org/v2/competitions/CL/standings",
+    options
+  );
+  console.log(res);
+  const data = await res.json();
+  console.log(data);
+};
+//fetchFootballData();
+// ZADANIE 1
+// a) znajdź i wypisz nazwę rozgrywek
+// b) znajdź i wypisz sezon jako
+
+// 1. XMLHTTPREQEUST
+// 2. FETCH API + THEN
+// 3. FETCH API + ASYNC/AWAIT
+
+// const fetchJSON = () => {
+//   fetch("https://jsonplaceholder.typicode.com/posts/5")
+//     .then((res) => {
+//       console.log(res);
+//       return res.json();
+//     })
+//     .then((data) => console.log(data));
+// };
+// fetchJSON();
+// then chain
+
+async function AsyncGetHttpRequest() {
+  const options = {
+      method: "GET",
+      headers: {
+          "X-Auth-Token": "bcdb7c1e2298472c9d71425346d5cc70",}
+      }
+  
+  const response = await fetch("http://api.football-data.org/v4/competitions/", options);
+  const data = await response.json();
+  console.log(data);
+}
+AsyncGetHttpRequest();
